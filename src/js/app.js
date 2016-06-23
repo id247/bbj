ed4fad9eee4fb6fc85b3b8b7b6d6bfd224adc9ae
simplecss <simplecss@yandex.ru>
2016-06-23 13:41:33 +0300

    map

--- src/js/app.js
+++ src/js/app.js
@@ -99,6 +99,120 @@ export default (function App(window, document, $){
 
 	}
 
+	/*
+		submit form
+	*/
+
+	function form(){		
+
+		$.extend($.validator.messages, {
+			required: 'Это поле обязательно для заполнения.',
+			remote: 'Please fix this field.',
+			email: 'Введите корректный e-mail адрес.',
+			url: 'Please enter a valid URL.',
+			date: 'Please enter a valid date.',
+			dateISO: 'Please enter a valid date (ISO).',
+			number: 'Введите число.',
+			digits: 'Допустимо вводить только цифры.',
+			creditcard: 'Please enter a valid credit card number.',
+			equalTo: 'Please enter the same value again.',
+			accept: 'Please enter a value with a valid extension.',
+			maxlength: jQuery.validator.format('Please enter no more than {0} characters.'),
+			minlength: jQuery.validator.format('Please enter at least {0} characters.'),
+			rangelength: jQuery.validator.format('Please enter a value between {0} and {1} characters long.'),
+			range: jQuery.validator.format('Please enter a value between {0} and {1}.'),
+			max: jQuery.validator.format('Please enter a value less than or equal to {0}.'),
+			min: jQuery.validator.format('Please enter a value greater than or equal to {0}.')
+		});
+
+		$('form').each( function(){
+
+			const $form = $(this);
+			const $button = $form.find('button[type="submit"]');
+			const $success = $form.find('.order-form__success');
+			
+			$success.hide();
+
+			$form.validate({
+			});
+
+			$form.on('submit', function(e){
+
+				e.preventDefault();
+
+				const form = e.target;
+
+				if ( !$(form).valid() ){
+					return false;
+				}
+
+				$button.text('Отправка данных...');
+				$button.attr('disabled', true);
+
+				$.ajax({
+					url: $form.attr('action'), 
+				    method: 'POST',
+				    data: $form.serialize(),
+				    dataType: 'json',
+				    success: function( response ) {
+				    	console.log(response);
+						$success.html('Спасибо! Ваша заявка была успешно отправлена!');
+						$success.removeClass('order-form__success--error');	
+				    },
+				    error: function(xhr, ajaxOptions, error){
+				    	console.log('Data could not be saved.' + error.message);
+						$success.addClass('order-form__success--error');
+						$success.html('Ошибка сохранения данных, попробуйте еще раз. Если ошибка повторится - свяжитесь с нами.');
+
+				    },
+				    complete: function(){					    	
+				    	$success.show();
+						$button.attr('disabled', false).text('Отправить заявку');			    	
+				    }
+				});				
+				
+				
+
+			});
+		});
+
+	}
+
+	function map(){
+		const $areas = $('.svg-map__area');
+		const $balloon = $('#map-ballon');
+		const $balloonTitle = $balloon.find('.map-balloon__title');
+		const $balloonText = $balloon.find('.map-balloon__text');
+
+		$areas.on('mouseenter', function(e){
+			const $area = $(this);
+			const title = $area.attr('mydata:title');
+			const text = this.getAttribute('mydata:text');
+			const offset = $area.offset();
+			const bBox = this.getBBox();
+
+			if (!title || !text){
+				return;
+			}
+
+			$balloonTitle.html(title);
+			$balloonText.html(text);
+
+			$balloon.addClass('map__balloon--visible');
+
+			$balloon.css({
+				top: offset.top + bBox.height / 2 - $balloon.outerHeight() / 2,
+				left: offset.left + bBox.width + 30,
+			});		
+
+			console.log(bBox);
+		});
+
+		$areas.on('mouseleave', function(e){
+			$balloon.removeClass('map__balloon--visible');
+		});
+	}
+
 	function init(){
 
 		if (!isMobile){
@@ -107,6 +221,8 @@ export default (function App(window, document, $){
 
 		scrollMeTo();
 		menu();
+		form();
+		map();
 	}
 
 	return {
